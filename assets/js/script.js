c633bc56c90bd81c5f5ed8a211d16094101fc116
LS-T <leo.s.teruel@gmail.com>
2021-03-29 23:10:43 -0400

    search input working

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,17 +6,19 @@ var temperature = $("#temp");
 var humidity = $("#humidity");
 var windSpeed = $("#windSpeed");
 var UvIndex = $("#uv");
+var fiveDayForecast = $(".fiveDayForecast");
 
+var date = moment().format("MM[/] DD[/] YYYY");
+console.log(date);
 
-var searchCity = citySearch.val();
-console.log(searchCity);
 
 var apiKey = "1ebd3e88b4147deeadc030e6248c294d";
-var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&units=imperial&appid=" + apiKey;
 
 
 
-function getApi(){
+
+function getApi(City){
+    var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + City + "&units=imperial&appid=" + apiKey;
     fetch(requestURL)
         .then(function(response){
             return response.json();
@@ -26,7 +28,18 @@ function getApi(){
                 console.log(data.main.temp);
                 console.log(data.main.humidity);
                 console.log(data.wind.speed);
+                cityName.text(data.name + " " + date + data.weather.id);
+                temperature.text(data.main.temp + " "+ "°F");
+                humidity.text(data.main.humidity + " " + "%");
+                windSpeed.text(data.wind.speed + " " + "MPH");
 
+                
+                
+                
+                 
+        
+
+                
             })
 }
 
@@ -34,12 +47,25 @@ function getApi(){
 
 
 
-
 searchBtn.on("click", function(){
-    
+    var searchCity = citySearch.val();
+    console.log(searchCity);
 
-    getApi();
+    getApi(searchCity);
 
     
 })
 
+
+// var template = `
+// <div class="card mx-2" style="width: 15rem;">
+//         <div class="card-body">
+//           <h5 class="card-title">${data.name}</h5>
+//           <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+//           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//           <a href="#" class="card-link">Card link</a>
+//           <a href="#" class="card-link">Another link</a>
+//         </div>
+//       </div>
+// `
+// document.querySelector(".fiveDayForecast").innerHTML += template
