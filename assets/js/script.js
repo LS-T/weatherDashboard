9849c186bce0f5e07d7855b2b2845bb1e12ae97b
LS-T <leo.s.teruel@gmail.com>
2021-03-30 16:45:01 -0400

    got card template working

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,21 @@ var temperature = $("#temp");
 var humidity = $("#humidity");
 var windSpeed = $("#windSpeed");
 var UvIndex = $("#uv");
+var fiveDayHeader = $("#fiveDayHeader");
 var fiveDayForecast = $(".fiveDayForecast");
 
+// Grab todays date
 var date = moment().format("MM[/] DD[/] YYYY");
 console.log(date);
 
 
 var apiKey = "1ebd3e88b4147deeadc030e6248c294d";
+
+var index = [0,1,2,3,4];
+
+
+
+
 
 
 
@@ -33,9 +41,36 @@ function getApi(City){
                 humidity.text(data.main.humidity + " " + "%");
                 windSpeed.text(data.wind.speed + " " + "MPH");
 
+                // Set text for header that will include 5 cards
+                fiveDayHeader.text(" 5 Day Forecast");
+
+                // Created the card template that was once hard coded
+                var cardTemplate = `
+                <div class="card mx-2" style="width: 15rem;">
+                        <div class="card-body">
+                          <h5 class="card-title">${data.name}</h5>
+                          <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="#" class="card-link">Card link</a>
+                          <a href="#" class="card-link">Another link</a>
+                        </div>
+                      </div>
+                `
                 
-                
-                
+                for (var i = 0; i < index.length; i++){
+                    
+
+                    // grab class fiveDayForecast and set innerHTML to cardTemplate
+                    document.querySelector(".fiveDayForecast").innerHTML += cardTemplate;
+
+
+
+
+
+
+
+                    
+                }
                  
         
 
@@ -57,15 +92,3 @@ searchBtn.on("click", function(){
 })
 
 
-// var template = `
-// <div class="card mx-2" style="width: 15rem;">
-//         <div class="card-body">
-//           <h5 class="card-title">${data.name}</h5>
-//           <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-//           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-//           <a href="#" class="card-link">Card link</a>
-//           <a href="#" class="card-link">Another link</a>
-//         </div>
-//       </div>
-// `
-// document.querySelector(".fiveDayForecast").innerHTML += template
