fc040ed24d80d343caa4d51a17845cca88559072
LS-T <leo.s.teruel@gmail.com>
2021-03-29 22:07:30 -0400

    data not loading

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,45 @@
-console.log("hello world");
-// var requestURL = "api.openweathermap.org/data/2.5/weather?q=atlanta&appid=1ebd3e88b4147deeadc030e6248c294d"
-// var apiKey = "1ebd3e88b4147deeadc030e6248c294d";
-// var city = "Atlanta"
+// DOM variables
+var searchBtn = $("#Search");
+var citySearch = $(".searchCity")
+var cityName = $(".cityName");
+var temperature = $("#temp");
+var humidity = $("#humidity");
+var windSpeed = $("#windSpeed");
+var UvIndex = $("#uv");
 
 
-// function getApi(){
-//     fetch(requestURL)
-//         .then(function(response){
-//             return response.json();
-//         })
-//             .then(function(data){
-//                 console.log(data);
-//             })
-// }
+var searchCity = citySearch.val();
+console.log(searchCity);
 
-// getApi();
+var apiKey = "1ebd3e88b4147deeadc030e6248c294d";
+var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&units=imperial&appid=" + apiKey;
+
+
+
+function getApi(){
+    fetch(requestURL)
+        .then(function(response){
+            return response.json();
+        })
+            .then(function(data){
+                console.log(data);
+                console.log(data.main.temp);
+                console.log(data.main.humidity);
+                console.log(data.wind.speed);
+
+            })
+}
+
+
+
+
+
+
+searchBtn.on("click", function(){
+    
+
+    getApi();
+
+    
+})
+
